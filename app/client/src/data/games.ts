f9517568475d1e90f79a3b86d1056b09034f012e
stan-dot <stanislawmalinowski09@gmail.com>
2022-06-03 13:55:37 +0100

    added more games

--- app/client/src/data/games.ts
+++ app/client/src/data/games.ts
@@ -4,5 +4,25 @@ export const availableGames: GameCard[] = [
   {
     folder: 'cat-facts',
     componentName: 'CatFacts'
-  }
+  },
+  // {
+  //   folder: 'eliza',
+  //   componentName: 'Eliza',
+  // },
+  // {
+  //   folder: 'minesweeper',
+  //   componentName: 'Minesweeper',
+  // },
+  {
+    folder: 'raw-canvas',
+    componentName: 'CanvasGame',
+  },
+  // {
+  //   folder: 'snake',
+  //   componentName: 'Snake',
+  // },
+  {
+    folder: 'tictactoe',
+    componentName: 'TicTacToe',
+  },
 ];
