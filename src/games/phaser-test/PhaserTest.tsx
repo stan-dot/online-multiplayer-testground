b87c4179598be67d265b370d5a113d6ff219befa
stan-dot <stanislawmalinowski09@gmail.com>
2022-11-14 22:39:58 +0000

    moved todo to phaser

--- src/games/phaser-test/PhaserTest.tsx
+++ src/games/phaser-test/PhaserTest.tsx
@@ -1,5 +1,14 @@
 
 
+// todo phaser stuff
+// https://phaser.io/tutorials/getting-started-phaser3
+
+// Working with rexAwait async preloader.
+// Dynamically adding game objects to scene.
+// Events center and passing in init data on scenes.
+// Dynamically switching between scenes.
+// Handling json tilemaps.
+// Custom collision callbacks.
 export default function PhaserTest(): JSX.Element {
   return <div id="phaserTest">
     <p>Testing PhaserJS</p>
