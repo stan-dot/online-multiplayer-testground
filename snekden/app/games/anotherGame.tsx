a22803099dcaefd4b80fff8af6cb63f8557be7a4
stan-dot <stanislawmalinowski09@gmail.com>
2022-12-26 23:04:23 +0100

    doing the first game tutorial

---
+++ snekden/app/games/anotherGame.tsx
@@ -0,0 +1,51 @@
+
+
+import Phaser from "phaser";
+
+export const config: Phaser.Types.Core.GameConfig = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 200 }
+    }
+  },
+  scene: {
+    preload: preload,
+    create: create
+  }
+};
+
+
+function preload(this: Phaser.Scene) {
+  this.load.image('sky', 'assets/sky.png');
+  this.load.image('ground', 'assets/platform.png');
+  this.load.image('star', 'assets/star.png');
+  this.load.image('bomb', 'assets/bomb.png');
+  this.load.spritesheet('dude',
+    'assets/dude.png',
+    { frameWidth: 32, frameHeight: 48 }
+  );
+}
+
+function create(this: Phaser.Scene) {
+  this.add.image(400, 300, 'sky');
+
+  const particles = this.add.particles('red');
+
+  const emitter = particles.createEmitter({
+    speed: 100,
+    scale: { start: 1, end: 0 },
+    blendMode: 'ADD'
+  });
+
+  const logo = this.physics.add.image(400, 100, 'logo');
+
+  logo.setVelocity(100, 200);
+  logo.setBounce(1, 1);
+  logo.setCollideWorldBounds(true);
+
+  emitter.startFollow(logo);
+}
--- snekden/app/page.tsx
+++ snekden/app/page.tsx
@@ -1,11 +1,21 @@
 'use client';
+import { useEffect } from "react";
 import { config } from "./games/testgame";
 
 export default function HomePage() {
-  const game = new Phaser.Game({ ...config, parent: 'game-content' });
+
+  const game = useGame();
+
   return <div>
     <h1> Home Page</h1>
     <p>Some content</p>
     <div id="game-content"></div>
   </div>
+}
+
+function useGame() {
+  useEffect(() => {
+    const game = new Phaser.Game({ ...config, parent: 'game-content' });
+    return () => { game; };
+  }, []);
 }
