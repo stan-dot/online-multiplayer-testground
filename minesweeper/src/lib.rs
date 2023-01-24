ed6d68a853afb4a5236feddcb4a88e9030c11a63
stan-dot <stanislawmalinowski09@gmail.com>
2023-01-24 22:35:54 +0000

    work on the display

--- minesweeper/src/lib.rs
+++ minesweeper/src/lib.rs
@@ -1,8 +1,16 @@
 mod random;
 use random::random_range;
-use std::collections::HashSet;
+use std::{
+    collections::HashSet,
+    fmt::{Display, Write},
+};
 
 pub type Position = (usize, usize);
+
+pub enum OpenResult {
+    Mine,
+    NoMine(u8),
+}
 
 #[derive(Debug)]
 struct Minesweeper {
@@ -11,6 +19,26 @@ struct Minesweeper {
     open_fields: HashSet<Position>,
     mines: HashSet<Position>,
     flagged_fields: HashSet<Position>,
+}
+
+impl Display for Minesweeper {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for y in 0..self.height {
+            for x in 0..self.width {
+                let pos = (x, y);
+                if !self.open_fields.contains(&pos) {
+                    f.write_str("🟪 ")?;
+                } else if self.mines.contains(&pos) {
+                    f.write_str("💣")?;
+                } else {
+                    write!(f, "{}", self.neighboring_mines(pos))?;
+                }
+            }
+
+            f.write_char('\n')?;
+        }
+        Ok(())
+    }
 }
 
 impl Minesweeper {
@@ -31,6 +59,35 @@ impl Minesweeper {
             flagged_fields: HashSet::new(),
         }
     }
+
+    fn iter_neighbors(&mut self, (x, y): Position) -> impl Iterator<Item = Position> {
+        let width = self.width;
+        let height = self.height;
+        (x.min(1) - 1..=(x + 1).min(width - 1))
+            .flat_map(move |i| (y.min(1) - 1..=(y + 1).min(height - 1)).map(move |j| (i, j)))
+            .filter(move |&pos| pos != (x, y))
+    }
+
+    fn neighboring_mines(&mut self, pos: Position) -> u8 {
+        let num: usize = self
+            .iter_neighbors(pos)
+            .filter(|pos| self.mines.contains(pos))
+            .count();
+        // u8::from(num)
+        num as u8
+        // as u8;
+    }
+
+    pub fn open(&mut self, position: Position) -> OpenResult {
+        self.open_fields.insert(position);
+        let is_mine = self.mines.contains(&position);
+
+        if is_mine {
+            OpenResult::Mine
+        } else {
+            OpenResult::NoMine(0)
+        }
+    }
 }
 
 #[cfg(test)]
@@ -39,7 +96,9 @@ mod tests {
 
     #[test]
     fn test() {
-        let ms = Minesweeper::new(10, 10, 5);
+        let mut ms = Minesweeper::new(10, 10, 5);
+
+        ms.open((5, 5));
         println!("{:?}", ms);
     }
 }
