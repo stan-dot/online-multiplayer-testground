715dfe89412fde58b6403a3d3eaebf713c09e01e
stan-dot <stanislawmalinowski09@gmail.com>
2023-01-25 19:28:44 +0000

    it failed another time

--- snekden/next.config.js
+++ snekden/next.config.js
@@ -2,20 +2,21 @@
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
-    turbotrace: true,
+    // turbotrace: true,
     appDir: true,
 
   },
   webpack: {
-    webpack: (config) => {
-      const experiments = config.experiments || {}
-      config.experiments = { ...experiments, asyncWebAssembly: true }
-      config.output.assetModuleFilename = 'static/[hash][ext]'
-      config.output.publicPath = '/_next/'
-      config.module.rules.push({
-        test: /\.wasm/,
-        type: 'asset/resource'
-      })
+    webpack: function (config) {
+      // const experiments = config.experiments || {}
+      // config.experiments = { ...experiments, asyncWebAssembly: true }
+      config.experiments = { asyncWebAssembly: true }
+      // config.output.assetModuleFilename = 'static/[hash][ext]'
+      // config.output.publicPath = '/_next/'
+      // config.module.rules.push({
+      //   test: /\.wasm/,
+      //   type: 'asset/resource'
+      // })
       return config
     }
   }
