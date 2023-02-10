81771b9697e0597ef857bc7de45d630b372ef66d
stan-dot <stanislawmalinowski09@gmail.com>
2023-02-10 07:27:25 +0000

    fixed the ur-game useref error with use client lol

--- snekden/app/games/royal-game-of-ur/page.tsx
+++ snekden/app/games/royal-game-of-ur/page.tsx
@@ -1,19 +1,18 @@
+"use client";
 import AnotherTutorial from "./AnotherTutorial";
 import Toggler from "./toggler";
-
 
 export default function RoyalGame() {
   return <div id="royalGameOfUrContainer">
     <p>game</p>
-    {/* <Toggler /> */}
-    {/* <AnotherTutorial/> */}
+    <Toggler />
+    <AnotherTutorial/>
   </div>
 }
 
   // todo define ruleset 
   // todo display
   // todo do the piping from one to the other
-  // todo something's not wrong
 // inspired by this
 // https://www.youtube.com/watch?v=WZskjLq040I
 
